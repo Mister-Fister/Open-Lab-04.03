a5bb110bf652832f1471292aa2f104e4888eeef8
Matúš <matusskor@gmail.com>
2020-10-30 09:56:17 +0100

    zmeny

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,27 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
-        }
+            if(original.Length < 2)
+            {
+                return original;
+            }
+
+            char[] a = new char[original.Length - 1];
+            for(int i = 0; i < original.Length - 1; i++)
+            {
+                a[i] = original[i];
+            }
+            Array.Reverse(a);
+            char[] b = new char[a.Length - 1];
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                b[i] = a[i];
+            }
+            Array.Reverse(b);
+            string vysl = String.Join("", b);
+            return vysl;
+        } 
+        
+
     }
 }
